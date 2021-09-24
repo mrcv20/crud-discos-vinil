49daea5502c5c74744010f9e35f1ddbaf3855899
mrcv20 <marcos.valentee20@gmail.com>
2021-09-23 23:59:25 -0400

    versao 01 da api

--- app/api/api01.py
+++ app/api/api01.py
@@ -1,16 +1,12 @@
-from flask_restx import Namespace, Resource, fields, Api
+from flask_restx import Namespace, Resource, fields
 from flask import jsonify, request, json, sessions
-from myapp.models import VinylDisc
+from app.models import VinylDisc, db
+from app.api.serializers import discos_schema
 
-ns = Namespace('loja vinil', description='API - CRUD de uma loja de disco de vinil')
+ns = Namespace('disco', description='Disco de Vinil')
 
-disco_vinil = ns.model(
-    'Cadastro do disco de vinil', {
-        'nome': fields.String(
-            description='Nome do artista',
-            required=True,
-            min_length=5
-        ),
+cria_disco_vinil_request = ns.model(
+    'Variáveis para criação do disco', {
         'titulo': fields.String(
             required=True,
             description='Título do disco de vinil',
@@ -22,21 +18,73 @@ disco_vinil = ns.model(
         'valor': fields.Float(
             description='Preço do disco',
             required=True
+        
+        ),
+        'artista': fields.String(
+            description='Nome do artista',
+            required=True
         )
     }
 )
 
-@ns.route('/disco', doc={"description": 'Registar um disco de vinil'})
+cria_disco_vinil_response = ns.model(
+    'Resposta da criação do disco',{
+        'id': fields.String(
+            description="Identifcador único do disco."
+        )
+    }
+)
+
+@ns.route('/', doc={"description": 'Registar um disco de vinil'})
 class RegistraDisco(Resource):
-    @ns.expect(disco_vinil, validate=True)
+    @ns.expect(cria_disco_vinil_request, validate=True)
     def post(self):      
         try:
             data = request.get_json()
-            return "ok"
+            print(type(data['valor']))
+            valor = data['valor']
+            novo_disco = VinylDisc(titulo=data['titulo'], genero=data['genero'], valor=data['valor'], artista=data['artista'])
+            db.session.add(novo_disco)
+            db.session.commit()
         except Exception as error:
             print(error)
         finally:
-            return jsonify(f"Consulte o status")
+            return jsonify(f"Dados inseridos!")
+
+@ns.route('/', doc={"description": 'Consulta todos os discos de vinil'})
+class ConsultaDisco(Resource):
+    def get(self):    
+        try:  
+            discos = VinylDisc.query.all()
+            return discos_schema.dump(discos)
+        except Exception as error:
+            return error
+        finally:
+            return jsonify("Carregado com sucesso!")
+
+@ns.route('/<id>', doc={"description": 'Deleta um disco da base de dados'})
+class Disco(Resource):
+    def delete(self, id):   
+        try:   
+            disco = VinylDisc.query.filter(VinylDisc.id == id).delete()
+            db.session.commit()
+            return jsonify("Deletado!")
+        except Exception as error:
+            return error
+
+    def put(self, id):   
+        try:   
+            data = request.get_json()
+            disco = VinylDisc.query.filter(VinylDisc.id == id).first()
+            disco.genero = data['genero']
+            disco.titulo = data['titulo']
+            disco.valor = data['valor']
+            disco.artista = data['artista']
+            db.session.commit()
+            return jsonify("Deletado!")
+        except Exception as error:
+            return error
+
 
 def ns_api(api):
     api.add_namespace(ns)
